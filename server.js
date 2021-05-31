640f9e86c918ca29386bf8bcc1690d70646e55b7
Trey Hernandez <Tahrnndz@gmail.com>
2021-05-31 17:56:59 -0500

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
 const db = knex({
-  client: "pg",
+  client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: false
