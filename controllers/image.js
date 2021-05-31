7a769cccb8b2dec62bf60749d523408fee241906
Trey Hernandez <Tahrnndz@gmail.com>
2021-05-31 16:42:46 -0500

    prep for heroku deployment

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'cd87948fcf2e481392d4b47daf1f1b81'//'API Key Here'
+  });
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to call api'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage: handleImage, 
+    handleApiCall: handleApiCall
+}
